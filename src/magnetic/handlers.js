c48d743673c5fed8e602219e6381ace3b13d60d7
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-05-22 20:27:16 -0700

    Added pin parameter

--- src/magnetic/handlers.js
+++ src/magnetic/handlers.js
@@ -1,10 +1,12 @@
 const gpio = require('rpi-gpio');
 
-exports.readInput = function() {
-  gpio.read(pin, function(err, value) {
-    if (err)
-      console.log('Error reading sensor: ', err);
-    else
-      console.log('The value is ' + value);
-  });
+exports.readInput = function(pin) {
+  return function() {
+    gpio.read(pin, function(err, value) {
+      if (err)
+        console.log('Error reading sensor: ', err);
+      else
+        console.log('The value is ' + value);
+    });
+  };
 };
--- src/magnetic/index.js
+++ src/magnetic/index.js
@@ -6,8 +6,7 @@ exports.magneticSensorCollector = function(pin) {
 
   gpio.setMode(gpio.MODE_BCM);
   setInterval(function() {
-    gpio.setup(pin, gpio.DIR_IN, gpio.EDGE_BOTH, handlers.readInput);
-
+    gpio.setup(pin, gpio.DIR_IN, gpio.EDGE_BOTH, handlers.readInput(pin));
   }, 1000);
 
 };
