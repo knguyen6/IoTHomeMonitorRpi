6e38eb1ddd6c19f8b1b8f43add37077a522dec0d
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-05-26 12:51:05 +0900

    something wrong with date still

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
-const camera = require('./camera');
-const magnetic = require('./magnetic');
-const buzzer = require('./buzzer');
-const motion = require('./motion');
-const temperature = require('./temperature');
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3();
 
-const pins = require('./pins'); // ex) PINS.motion['gpio']
+// const camera = require('./camera');
+// const magnetic = require('./magnetic');
+// const buzzer = require('./buzzer');
+// const motion = require('./motion');
+// const temperature = require('./temperature');
+
+// const pins = require('./pins'); // ex) PINS.motion['gpio']
 
 /* Application logic goes here and re-use separate modules */
 
@@ -13,12 +16,25 @@ const pins = require('./pins'); // ex) PINS.motion['gpio']
 //read temp sensor on GPIO 17
 // temperature.temperatureSensor();
 
-let pirSensor = motion.init(pins.motion['gpio']);
-pirSensor.watch((err, value) => {
-	if (value) {
-		let timeNow = new Date().toLocaleString();
-		console.log('movement detected - ${timeNow}');
-		console.log('camera recording...');
-		camera.start();
-	}
+// let pirSensor = motion.init(pins.motion['gpio']);
+// pirSensor.watch((err, value) => {
+// 	if (value) {
+// 		let timeNow = new Date().toLocaleString();
+// 		console.log('movement detected - ${timeNow}');
+// 		camera['photo'].start();
+
+// 		// TODO asynchronously invoke aws and send photo
+
+// 		console.log('camera recording...');
+// 		camera['video'].start();
+// 	}
+// });
+
+// camera['photo'].start();
+
+console.log('sdfkushfdklsj');
+
+S3.listBuckets((err, data) => {
+	console.log(`err: ${err}`);
+	console.log(`data: ${data}`);
 });
--- src/camera/index.js
+++ src/camera/index.js
@@ -5,13 +5,21 @@ const RaspiCam = require('raspicam');
 const Handlers = require('./handlers');
 const Utils = require('./utils');
 
-const camera = new RaspiCam({
+const video = new RaspiCam({
   mode: 'video',
   output: Utils.fileOutput('intruder.mp4'),
   timeout: 300000
 });
 
-camera.on('started', Handlers.onStart);
-camera.on('stop', Handlers.onStop);
+const photo = new RaspiCam({
+	mode: 'photo',
+	output: Utils.fileOutput('intruder.png')
+});
 
-module.exports = camera;
+video.on('started', Handlers.onStart);
+video.on('stop', Handlers.onStop);
+
+module.exports = {
+	video: video,
+	photo: photo
+};
