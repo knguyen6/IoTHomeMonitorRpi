51443baf6039beaa7e7a4f4627efd29af5b77de0
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-05-27 13:48:20 -0700

    beginning refactoring changes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
-const AWS = require('aws-sdk');
-const S3 = new AWS.S3();
+/* Native libraries */
+const fs = require('fs');
 
-// const camera = require('./camera');
-// const magnetic = require('./magnetic');
-// const buzzer = require('./buzzer');
-// const motion = require('./motion');
-// const temperature = require('./temperature');
+/* AWS dependencies */
+const Aws = require('aws-sdk');
+const IoT = require('aws-iot-device-sdk');
 
-// const pins = require('./pins'); // ex) PINS.motion['gpio']
+/* Hardware module dependencies */
+const camera = require('./camera');
+const magnetic = require('./magnetic');
+const buzzer = require('./buzzer');
+const gpio = require('onoff').Gpio;
+const temperature = require('./temperature');
+
+/* Instantiate modules */
+const DEVICE = IoT.device(require('./credentials'));
+const S3 = new Aws.S3();
+
+const pins = require('./pins'); // ex) PINS.motion['gpio']
 
 /* Application logic goes here and re-use separate modules */
 
@@ -16,25 +25,52 @@ const S3 = new AWS.S3();
 //read temp sensor on GPIO 17
 // temperature.temperatureSensor();
 
-// let pirSensor = motion.init(pins.motion['gpio']);
-// pirSensor.watch((err, value) => {
-// 	if (value) {
-// 		let timeNow = new Date().toLocaleString();
-// 		console.log('movement detected - ${timeNow}');
-// 		camera['photo'].start();
+let pirSensor = new gpio(pins.motion['gpio'], 'in', 'both');
+pirSensor.watch((err, value) => {
+	console.log('===========');
+	console.log(`err ${err}`);
+	console.log(`value ${value}`);
+	console.log('===========');
 
-// 		// TODO asynchronously invoke aws and send photo
+	if (value) {
+		let now = new Date();
+		let timenow = `${now.toLocaleDateString()} - ${now.toLocaleTimeString()}`;
+		console.log(`movement detected - ${timenow}`);
+		camera['photo'].start();
 
-// 		console.log('camera recording...');
-// 		camera['video'].start();
+		// TODO asynchronously invoke aws and send photo
+
+		// console.log('camera recording...');
+		// camera['video'].start();
+	}
+});
+
+/********************AWS IoT Connection Test********************/
+// DEVICE.on('connect', (err, data) => {
+// 	console.log(`err ${err}`);
+// 	console.log(`data ${data}`);
+// 	if (err) {
+// 		throw err;
+// 	} else {
+// 		console.log('connected');
 // 	}
 // });
 
+/********************CAMERA TEST WITH S3********************************/
 // camera['photo'].start();
+// camera['photo'].on('read', (err, filename) => {
+// 	let location = camera['photo'].get('output');
+// 	camera['photo'].stop();
 
-console.log('sdfkushfdklsj');
+// 	fs.readFile(location, (err, fileData) => {
+//   	if (err) throw err;
 
-S3.listBuckets((err, data) => {
-	console.log(`err: ${err}`);
-	console.log(`data: ${data}`);
-});
+// 		S3.putObject({
+// 			Bucket: 'tcss-573',
+// 			Key: 'intruder.png',
+// 			Body: fileData
+// 		}, (err, s3Data) => {
+// 			console.log(s3Data);
+// 		});
+// 	});
+// });
--- src/camera/handlers.js
+++ src/camera/handlers.js
@@ -4,6 +4,12 @@ exports.onStart = function(error, timestamp) {
   console.log(`camera recording [${timestamp}]`);
 };
 
-exports.onStop = function(timestamp) {
-  console.log(`camera recording done [${timestamp}]`);
+exports.onStop = function() {
+  console.log(`camera recording done`);
+};
+
+exports.onRead = function(err, timestamp) {
+	let now = new Date(timestamp);
+
+	console.log(`snapshot saved on ${now.toLocaleDateString()} - ${now.toLocaleTimeString()}`);
 };
