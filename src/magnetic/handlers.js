3c8bffb20695d794273ed1ed42db938b1889c50e
knguyen6 <rkamelot6262@gmail.com>
2017-05-29 20:34:54 -0700

    add callback to return data object for magnetic sensor data.

--- src/magnetic/handlers.js
+++ src/magnetic/handlers.js
@@ -1,18 +1,23 @@
 const gpio = require('rpi-gpio');
 
-exports.readInput = function(pin) {
+exports.readInput = function(pin, callback) {
   return function() {
     gpio.read(pin, function(err, value) {
-      if (err)
+      if (err){
         console.log('Error reading sensor: ', err);
-      else 
+	callback();
+	}
+      else {
 	translate(value);
-
+	callback(null, {'data':value});
+	}//else
     });
   };
 };
 
 
+//value = false: door close
+//value = true: door open
 function translate(value){
     if (value) console.log('Door Open, sensor value = ', value);
     else console.log('Door Close, sensor value = ', value);
--- src/magnetic/index.js
+++ src/magnetic/index.js
@@ -8,7 +8,10 @@ exports.magneticSensorCollector = function(pin) {
   console.log('======== start collecting magnetic door sensor ======');
 
   setInterval(function() {
-    gpio.setup(pin, gpio.DIR_IN, gpio.EDGE_BOTH, handlers.readInput(pin));
+    gpio.setup(pin, gpio.DIR_IN, gpio.EDGE_BOTH, handlers.readInput(pin, function(err, data){
+	if (data)
+		console.log('Door sensor data object: ', data);
+	}));
   }, 1000);
 
 };
