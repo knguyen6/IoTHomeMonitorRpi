657255b1687fae8d27303a55d204b02c98570815
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-05-29 17:10:59 -0700

    streaming working

--- src/app.js
+++ src/app.js
@@ -1,36 +1,30 @@
-/* Native libraries */
-const fs = require('fs');
-
-/* AWS dependencies */
-const Aws = require('aws-sdk');
-const IoT = require('aws-iot-device-sdk');
+const Aws   = require('aws-sdk');
+const IoT   = require('aws-iot-device-sdk');
+const gpio  = require('onoff').Gpio;
 
 /* Hardware module dependencies */
-const camera = require('./camera');
+// const camera = require('./camera');
 const magnetic = require('./magnetic');
+const camera  = require('./camera');
 const buzzer = require('./buzzer');
-const gpio = require('onoff').Gpio;
 const temperature = require('./temperature');
-
-/* Initialize function handlers */
-const rPiController = require('./controller');
 
 /* Pin settings */
 const pins = require('./pins'); // ex) PINS.motion['gpio']
 
 /* Instantiate modules */
-const DEVICE = IoT.device(require('./credentials'));
-const S3 = new Aws.S3();
+// const Device = IoT.device(require('./credentials'));
 
 /********************AWS IoT Connection Test********************/
-DEVICE.on('connect', rPiController['onDeviceConnected']);
+// Device.on('connect', rPiController['onDeviceConnected']);
 
 /********************Application logic********************/
-
 // magnetic.magneticSensorCollector(PINS['motion'].gpio);
 
 // read temp sensor on GPIO 17
 // temperature.temperatureSensor();
 
-let pirSensor = new gpio(pins.motion['gpio'], 'in', 'both');
-pirSensor.watch(rPiController['onCameraDetect']);
+// let pirSensor = new gpio(pins.motion['gpio'], 'in', 'both');
+// pirSensor.watch(rPiController['onCameraDetect']);
+
+camera['stream'].start();
--- src/camera/index.js
+++ src/camera/index.js
@@ -1,32 +1,63 @@
 'use strict';
 
-const RaspiCam = require('raspicam');
+const RaspiCam 	= require('raspicam');
+const io    		= require('socket.io-client');
+const Spawn 		= require('child_process').spawn;
+const fs 	  		= require('fs');
 
-const Handlers = require('./handlers');
-const Utils = require('./utils');
+const Handlers 	= require('./handlers');
+const Utils 		= require('./utils');
 
 const video = new RaspiCam({
   mode: 'video',
   output: Utils.fileOutput('intruder.mp4'),
-  timeout: 300000,
+  timeout: 300000, // 5 minutes
   verbose: true
-});
-
-const photo = new RaspiCam({
-	mode: 'photo',
-	output: Utils.fileOutput('intruder.png'),
-	verbose: true
 });
 
 video.on('started', Handlers.onStart);
 video.on('stop', Handlers.onStop);
 video.on('read', Handlers.onRead);
 
-photo.on('started', Handlers.onStart);
-photo.on('stop', Handlers.onStop);
-photo.on('read', Handlers.onRead);
+const photo = new RaspiCam({
+	mode: 'photo',
+	output: Utils.fileOutput('image-stream.png'),
+	width: 640,
+	height: 480,
+	timeout: 999999999,
+	timelapse: 100,
+	verbose: true
+});
+
+// TODO Change this URL to the EC2 server
+const SOCKET_SERVER_URL 	= 'http://192.168.1.14:3000';
+const MESSAGE_TOPIC 			= 'iot-home-intruder';
+
+function startStream() {
+	const Socket = io(SOCKET_SERVER_URL, { reconnection: true });
+	Socket.on('connect', () => {
+	  console.log('Raspberry PI successfully connected to Socket server. Ready to start streaming...');
+
+	  photo.start();
+	  photo.on('read', (err, timestamp) => {
+	    if (err) throw err;
+
+	    let imageLocation = photo.get('output');
+	    console.log('=======> Photo taken');
+	    console.log('=======> imageLocation:', imageLocation);
+	    fs.readFile(imageLocation, (err, imageStream) => {
+	      if (err) throw err;
+	      console.log('=======> Successfully read image file, emitting stream...');
+	      Socket.emit(MESSAGE_TOPIC, imageStream.toString('base64'));
+	    });
+	  });
+	});
+}
 
 module.exports = {
 	video: video,
-	photo: photo
+	photo: photo,
+	stream: {
+		start: startStream
+	}
 };
